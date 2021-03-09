4025610bc2318219c6f97e15a6f173840133032a
JonYang <a1109340909@gmail.com>
2021-03-09 15:02:16 +0800

    init

--- DuiEditor/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-
-// stdafx.cpp : 只包括标准包含文件的源文件
-// DuiEditor.pch 将作为预编译头
-// stdafx.obj 将包含预编译类型信息
-
-#include "stdafx.h"
-tagEditorConfig g_cfg;
-
-LsStringConverter __lsstring__convert__temp__instance__;
